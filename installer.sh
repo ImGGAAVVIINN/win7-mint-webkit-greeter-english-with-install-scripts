e99b31b0e72f88370ae3c3c8ec18d7a74511700c
Happy GrassBlock <68761233+ImGGAAVVIINN@users.noreply.github.com>
2023-04-12 12:39:19 +1000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 echo "This script will install the theme and configure lightdm to use it."
-echo "You might need to enter the root password."
+echo "This script will need tobe run as root"
 
-sudo cd "$(dirname "$0")"
+cd "$(dirname "$0")"
 
-sudo cat << EOF > /etc/lightdm/lightdm.conf.d/70-linuxmint.conf
+cat << EOF > /etc/lightdm/lightdm.conf.d/70-linuxmint.conf
 [SeatDefaults]
 user-session=cinnamon
 greeter-session=lightdm-webkit-greeter
 
 EOF
 
-sudo cd ../
-sudo mv win7-mint-webkit-greeter-english-with-install-scripts /usr/share/lightdm-webkit/themes/
-sudo chmod 777 -R /usr/share/lightdm-webkit/themes/win7-mint-webkit-greeter-english-with-install-scripts/
+cd ../
+mv win7-mint-webkit-greeter-english-with-install-scripts /usr/share/lightdm-webkit/themes/
+chmod 777 -R /usr/share/lightdm-webkit/themes/win7-mint-webkit-greeter-english-with-install-scripts/
 
-sudo lineTheme=$(grep "webkit-theme=" /etc/lightdm/lightdm-webkit-greeter.conf)
-sudo sed -i "s/$lineTheme/webkit-theme=win7-mint-webkit-greeter-english-with-install-scripts/g" \
-sudo /etc/lightdm/lightdm-webkit-greeter.conf
+lineTheme=$(grep "webkit-theme=" /etc/lightdm/lightdm-webkit-greeter.conf)
+sed -i "s/$lineTheme/webkit-theme=win7-mint-webkit-greeter-english-with-install-scripts/g" \
+/etc/lightdm/lightdm-webkit-greeter.conf
